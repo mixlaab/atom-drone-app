a27e74b76e81df11d4cde759bb8f3172c029ccbb
mixlaab <mixlaab.labs@gmail.com>
2021-07-15 19:56:05 -0500

    Initial commit: Real-time data with light sensor

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    GRAPHQL_SERVER: process.env.GRAPHQL_SERVER,
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { GRAPHQL_SERVER } = process.env;
+
+  const client = new ApolloClient({
+    link: new createHttpLink({
+      uri: GRAPHQL_SERVER,
+    }),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
